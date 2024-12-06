5738f8dc2421e99bcd2f0225b8530f0c79cbd601
Duhikim <macksa29@hotmail.com>
2024-12-06 23:44:32 +0900

    sandbox flush

--- 00 sandbox/소스.cpp
+++ 00 sandbox/소스.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <deque>
 using namespace std;
 
 int main() {
-    int testCase;
-    cin >> testCase;
-    while (testCase-- != 0) {
-        int repeatTime;
-        string word;
-        string S = "";
-        cin >> repeatTime >> word;
-        for (char c : word) {
-            for (int i = 0; i < repeatTime; i++) {
-                S.push_back(c);
-            }
-        }
-        cout << S << '\n';
-    }
+    vector<int> test = { 1,2,3,4,5,6,7,8,9 }; // 이 데이터는 지워지지 않음이 보장된다.
+
+    deque<int&> refVector = {};
+    refVector.push_back(test[8]);
+    cout << "참조 벡터에 어떤 값을 저장하였다. (참조) refVector[0] = " << refVector[0] << '\n';
+    test[8] = 19;
+    cout << "원본 데이터의 값을 9에서 19로 변경하였다. refVector[0] = " << refVector[0] << '\n';
+    int& a = refVector.back();
+    cout << "int& a는 refVector의 값(참조값)을 참조한다." << '\n';
+    int b = refVector.back();
+    cout << "int b는 refVector의 값(참조값)을 복사한다." << '\n';
     return 0;
 }
---
+++ 00 sandbox/소스2.cpp
@@ -0,0 +1,11 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//    int X;
+//    cin >> X;
+//    int answer = 0;
+//    while (X > 0) { answer += X & 1; X >>= 1; }
+//    cout << answer << '\n';
+//    return 0;
+//}
