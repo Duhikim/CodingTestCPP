d67e1c773e2392f615bfa8f187a3eced0e5c82af
Duhikim <macksa29@hotmail.com>
2024-12-17 11:56:59 +0900

    Study sorting and compare function in sandbox

--- 00 sandbox/소스.cpp
+++ 00 sandbox/소스.cpp
@@ -3,25 +3,62 @@
 #include <vector>
 #include <deque>
 #include <list>
+#include <algorithm>
 using namespace std;
+
+class MyClass {
+public:
+    int faceNum;
+    int innerNum;
+
+    MyClass(int i) {
+        faceNum = i;
+        innerNum = 5 - i;
+    }
+};
+
+bool compare(MyClass A, MyClass B) {
+    int a = A.faceNum, b = B.faceNum;
+    cout << std::boolalpha << "[ a : " << a << " , b : " << b << " , a <= b : " << (bool)(a <= b) << " ]" << '\n';
+    return a < b;
+}
 
 int main() {
    
-    vector<int> a = { 1,2,3,4,5 };
-    vector<int> b = a;
-    for (int num : b) cout << num; cout << '\n';
+    vector<MyClass> aVec;
 
-    a[4] = 0;
-    for (int num : a) cout << num; cout << '\n';
-    for (int num : b) cout << num; cout << '\n';
+    MyClass temp1(3);
+    aVec.push_back(temp1);
+    MyClass temp2(2);
+    aVec.push_back(temp2);
+    MyClass temp3(5);
+    aVec.push_back(temp3);
+    MyClass temp4(1);
+    aVec.push_back(temp4);
+    MyClass temp5(4);
+    aVec.push_back(temp5);
+    MyClass temp6(6);
+    temp6.faceNum = 3;    
+    aVec.push_back(temp6);
 
-    list<int> c = { 1,2,3,4,5 };
-    list<int> d = c;
-    for (int num : d) cout << num; cout << '\n';
+    cout << "Before Sort\n";
+    for (int i = 0; i < 6; i++) {
+        cout << aVec[i].faceNum << " , ";
+    }cout << '\n';
+    for (int i = 0; i < 6; i++) {
+        cout << aVec[i].innerNum << " , ";
+    }cout << '\n';
+    
+    sort(aVec.begin(), aVec.end(), compare);
 
-    c.push_back(10);
-    for (int num : c) cout << num; cout << '\n';
-    for (int num : d) cout << num; cout << '\n';
+
+    cout << "After Sort\n";
+    for (int i = 0; i < 6; i++) {
+        cout << aVec[i].faceNum << " , ";
+    }cout << '\n';
+    for (int i = 0; i < 6; i++) {
+        cout << aVec[i].innerNum << " , ";
+    }cout << '\n';
     
     return 0;
 }
