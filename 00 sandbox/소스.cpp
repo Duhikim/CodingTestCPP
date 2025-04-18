19b9d14f584ad654471c5dc6e896f0633db5a530
Duhikim <macksa29@hotmail.com>
2025-04-19 01:12:57 +0900

    double point test

--- 00 sandbox/소스.cpp
+++ 00 sandbox/소스.cpp
@@ -1,33 +1,14 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <deque>
-#include <list>
-#include <algorithm>
 using namespace std;
 
-long long F(int n) {
-    if (n == 0) return 1;
-    if (n == 1) return 1;
-
-    long long prev_prev = 1;
-    long long  prev = 1;
-    long long curr = 0;
-    for (int i = 2; i <= n; i++) {
-        curr = prev + prev_prev;
-        prev_prev = prev;
-        prev = curr;
-        
-    }
-    return curr;
-}
-
 int main() {
-    int n;
-    while (cin >> n && n >= 0) {
-        cout << "Fibonacci[" << n << "] = " << F(n) << '\n';
-    }
-    cout << "프로그램 종료\n";
-   
+    
+
+    int num = 7;
+    int* p = &num;
+    int** pp = &p;
+
+    cout << *pp << '\n' << p << '\n' << &num << '\n';
+    
     return 0;
 }
