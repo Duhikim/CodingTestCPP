4aac14745ccb83b5d5d832ba2fb4e48e16af60f0
Duhi Kim <macksa29@hotmail.com>
2025-07-04 13:00:49 +0900

    [2025.7.4] BJ9372 Travel

---
+++ baekjoon/bj9372_travel/bj9372_travel/solution.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+	int T;
+	cin >> T;
+
+	for (int t = 0; t < T; t++) {
+
+		int N, M;
+		cin >> N >> M;
+
+		for(int m = 0; m < M; m++) {
+			int a, b;
+			cin >> a >> b;
+		}
+
+		cout << N - 1 << '\n';
+
+	}
+
+	return 0;
+}
