e275611757fe5404f77ab283a8b5ba38b06d3e23
Duhi Kim <macksa29@hotmail.com>
2025-07-04 16:43:57 +0900

    [2025.7.4] BJ30618 Donstructive

---
+++ baekjoon/bj30618_donstructive/bj30618_donstructive/solution2.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+//#include <vector>	
+//#include <string>	
+//#include <algorithm>
+//using namespace std;
+//
+//int main() {
+//	int N;
+//	cin >> N;
+//
+//	int num = 1;
+//	while (num < N) {
+//		cout << num << " "; num += 2;
+//		if (num == N) cout << num << " ";
+//	} num--;
+//	while (num > 2) {
+//		cout << num << " "; num -= 2;
+//	} cout << 2 << endl;
+//
+//	return 0;
+//}
